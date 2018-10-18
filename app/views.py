994d419130274a411a92c6268ada26b2dc5b18e7
araalifarooq <farooqp35h@gmail.com>
2018-10-18 19:46:55 +0300

    Add fetch single product module [Finishes #161256572]

--- app/views.py
+++ app/views.py
@@ -1,6 +1,9 @@
 from app import app
-from flask import request
+from flask import request, jsonify
 from .models.product import Product
+from app.validation import Validation
+
+validation = Validation()
 
 @app.route("/api/v1/products",methods=["POST"])
 #adding product
@@ -14,4 +17,14 @@ def add_product():
 def fetch_all_products():
     all_products = Product.fetch_all_products()
     return all_products 
+
+@app.route("/api/v1/products/<product_id>", methods=["GET"])
+# fetching a single product
+def fetch_single_product(product_id):
+    valid = validation.validate_input_type(product_id)
+    if valid:
+        return jsonify({"message":valid}), 400
+    product = Product(product_id)
+    single_product = product.fetch_single_product()
+    return single_product
 
